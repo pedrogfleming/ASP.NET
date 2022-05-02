68ed4afe5d9be40557a29f0299104cd6f8cc5ffe
Geraghty Fleming Pedro <pedrogfleming@gmail.com>
2022-05-02 15:02:43 -0300

    Part 9 MOVIES WEB APP MS:add validation done

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -6,15 +6,37 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(60,MinimumLength =3)]
+        [Required]
         public string? Title { get; set; }
 
         [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Must only use letters.
+        /// The first letter is required to be uppercase.
+        /// White spaces are allowed while numbers, and special characters are not allowed.
+        /// </summary>
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Genre { get; set; }
 
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        /// <summary>
+        /// Requires that the first character be an uppercase letter.
+        ///Allows special characters and numbers in subsequent spaces.
+        ///"PG-13" is valid for a rating, but fails for a "Genre".
+        /// </summary>
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
         public String? Rating { get; set; }
     }
 }
