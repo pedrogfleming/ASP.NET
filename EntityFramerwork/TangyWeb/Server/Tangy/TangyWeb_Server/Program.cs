3b1b6d1d162b0e96ca84da7d240cc642ad0e19b6
Pedro Fleming <pedro.fleming@globallogic.com>
2022-05-30 23:30:21 -0300

    Tangy Blazor App: Section 11 - Products - almost done,only it need to be fixed the rich texc box display component on product description

--- EntityFramerwork/TangyWeb/Server/Tangy/TangyWeb_Server/Program.cs
+++ EntityFramerwork/TangyWeb/Server/Tangy/TangyWeb_Server/Program.cs
@@ -1,15 +1,21 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
 using Tangy_Business.Repository;
 using Tangy_Business.Repository.IRepository;
 using Tangy_DataAccess.Data;
 using TangyWeb_Server.Data;
+using TangyWeb_Server.Service;
+using TangyWeb_Server.Service.IService;
+
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENCE KEY");
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -17,6 +23,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //We use scoped so only ONE service its created per request
 //Dependency injection
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IFileUpload, FileUpLoad>();
 //Auto Mapping in dependency injection
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
--- EntityFramerwork/TangyWeb/Server/Tangy/Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ EntityFramerwork/TangyWeb/Server/Tangy/Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Tangy_DataAccess.Data
         {
 
         }
-        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
