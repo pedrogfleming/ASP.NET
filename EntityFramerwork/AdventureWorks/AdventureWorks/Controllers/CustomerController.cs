3c1b52f1f6f88944b38f0371efb2d542f1c0bb94
Pedro Fleming <pedro.fleming@globallogic.com>
2022-06-02 16:22:03 -0300

    Tangy Blazor App: Section 14 - Tangy Client-WASM - 119-124 done. Also started AdventureWorks API proyect

---
+++ EntityFramerwork/AdventureWorks/AdventureWorks/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using AdventureWorks.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Web.Http;
+
+namespace AdventureWorks.Controllers
+{
+    
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+       private readonly AdventureContext adventureContext;
+
+        public CustomerController(AdventureContext adventureContext)
+        {
+            this.adventureContext = adventureContext;
+        }
+
+        //[HttpGet(Name = "GetAllClients")]
+        //[Microsoft.AspNetCore.Mvc.HttpGet]
+        //public async Task<IActionResult> GetClients()
+        //{
+        //    return Ok(adventureContext.Customers.ToList());
+        //}
+
+        //[HttpGet(Name = "GetClientsPage/{Page:int}")]
+        //[Microsoft.AspNetCore.Mvc.HttpGet("{Page:int}")]
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        //public async Task<IActionResult> GetClientsPage([FromUri] QueryParams Page=null)
+        public async Task<IActionResult> GetClientsPage(
+            [FromQuery] int Page, 
+            [FromQuery] string? firstName="", 
+            [FromQuery] string? lastName="",
+            [FromQuery] string? Phone="")
+        {
+            //GetClientsPage([FromQuery] int? Page=null, [FromQuery]string firstName, [FromQuery] string lastName)
+            return Ok(adventureContext.Customers
+                .Where(c => c.FirstName == firstName && c.LastName == lastName && c.Phone == Phone)
+                .Skip(Page * 10 - 10)
+                .Take(10)
+                .ToList());
+
+            //return Ok(adventureContext.Customers.Skip(Page*10-10).Take(10).ToList());
+        }
+    }
+    public class QueryParams
+    {
+        public string Page { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
