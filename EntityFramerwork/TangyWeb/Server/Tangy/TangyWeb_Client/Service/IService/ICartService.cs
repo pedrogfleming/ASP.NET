03fbd94bb10ef716bdb2b687823a6b7afbfbc019
Pedro Fleming <pedro.fleming@globallogic.com>
2022-06-03 01:57:23 -0300

    Tangy Blazor App: Section 15 - Tangy Shopping Cart and Summary - 131-135 done. Pending to implement final Shopping cart UI

---
+++ EntityFramerwork/TangyWeb/Server/Tangy/TangyWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,10 @@
+﻿using TangyWeb_Client.ViewModel;
+
+namespace TangyWeb_Client.Service.IService
+{
+    public interface ICartService
+    {
+        Task DecrementCart(ShoppingCart shoppingCart);
+        Task IncrementCart(ShoppingCart shoppingCart);
+    }
+}
---
+++ EntityFramerwork/TangyWeb/Server/Tangy/Tangy_Common/SD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Common
+{
+    /// <summary>
+    /// We use constants rather than hardcoded string to improve escalation and refactoring
+    /// </summary>
+    public static class SD
+    {
+        public const string ShoppingCart = "ShoppingCart";
+    }
+}
